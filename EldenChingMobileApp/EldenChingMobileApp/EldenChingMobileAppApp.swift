81c60426896ec3b9494d3d3a6d5e3648ecc5b599
Leung-Kam-Ho <20051119d@connect.polyu.hk>
2022-03-25 23:02:23 +0800

    created mt xcode project

---
+++ EldenChingMobileApp/EldenChingMobileApp/EldenChingMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EldenChingMobileAppApp.swift
+//  EldenChingMobileApp
+//
+//  Created by Howard on 25/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct EldenChingMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
