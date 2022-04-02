ae7f3abede136cd4d932c7377dfb02204b952296
Leung-Kam-Ho <20051119d@connect.polyu.hk>
2022-04-02 18:05:54 +0800

    minor update on the layout and theme

--- EldenChingMobileApp/EldenChingMobileApp/SearchScene.swift
+++ EldenChingMobileApp/EldenChingMobileApp/SearchScene.swift
@@ -16,6 +16,7 @@ var SE70 = UIScreen.main.bounds.height*0.1049
 var SE50 = UIScreen.main.bounds.height*0.0749
 var SE40 = UIScreen.main.bounds.height*0.05997
 var SE60 = UIScreen.main.bounds.height*0.0899
+var SE100 = UIScreen.main.bounds.height*0.1499
 var SE170 = UIScreen.main.bounds.width*0.4533
 var SE189 = UIScreen.main.bounds.height*0.2742
 var SEscroll = UIScreen.main.bounds.height*0.772
@@ -36,28 +37,45 @@ struct ContentView: View {
     @State var isEditing = false
     var body: some View {
         ZStack{
-            RoundedRectangle(cornerRadius: 15).fill(Color(#colorLiteral(red: 0.2, green: 0.2, blue:0.2, alpha: 1))).frame(height:half_height*0.70).offset(y:-half_height*0.92).shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.5)), radius: 4, x: 0, y: 4)
+            ScrollView(.vertical, showsIndicators: false){
+                LazyVGrid(columns: twoColumnGrid, alignment:.center, spacing : 20) {
+                          // Display the item
+                 
+                    ForEach(category, id: \.self){ i in
+      
+                        if i.contains(search_text) || search_text == "" {
+ 
+                            cardView(name: i)
+                            
+                            
+                        }
+                    }.animation(Animation.ripple(index: 3) , value: search_text).offset(y: 50)
+                        
+                    
+                }
+            }.offset(y:screen_height*0.15).offset(y:isEditing ? -SE50*1.5 : 0).animation(Animation.ripple(index: 2) , value:isEditing)
+            RoundedRectangle(cornerRadius: 15)
+                .fill(Color(#colorLiteral(red: 0.2, green: 0.2, blue:0.2, alpha: 1)))
+                //.fill(Color.white)
+                .frame(height:half_height).offset(y:-half_height*1.08).shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.25)), radius: -4, x: 0, y: 4)
                 .animation(Animation.ripple(index: 3) , value:isEditing)
                 .offset(y: isEditing ? -SE50 : 0)
-            VStack{
-                
+            
                 ZStack {
-                //Rectangle 1
-                    Rectangle()
-                        .fill(Color(#colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)))
-                    .frame(width: screen_width, height: SE40   ).offset(y:-SE20)
     
-                    
                     //MetaChoice
-                    Text("MetaChoice").font(.custom("Sen Regular", size: 35)).foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                    Text("MetaChoice").font(.custom("Sen Regular", size: 35))
+                        .foregroundColor(Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)))
+                        .foregroundColor(Color.black)
+                        .frame(width: screen_width, height: SE40   )
                 }.animation(Animation.ripple(index: 3) , value:isEditing)
-                    .offset(y:isEditing ? -SE50*2 : 0)
+                .offset(y:isEditing ? -SE50*2 : 0).offset(y:-screen_height*0.46)
                 
                 ZStack {
                     //Rectangle 4
                     RoundedRectangle(cornerRadius: 15)
                     .fill(Color(#colorLiteral(red: 0.9019607901573181, green: 0.9019607901573181, blue: 0.9019607901573181, alpha: 1)))
-                    .frame(width: screen_width*0.9, height: SE60).shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.25)), radius: 4, x: 0, y: 4)
+                    .frame(width: screen_width*0.9, height: SE60).shadow(color: Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 0.25)), radius: -4, x: 0, y: 4)
                 //🔎
                     HStack{
                         Text("🔎").font(.custom("Inter Medium", size: 15))
@@ -73,29 +91,13 @@ struct ContentView: View {
                     
                 }.frame(height:SE82)
                     .animation(Animation.ripple(index: 3) , value:isEditing)
-                    .offset(y:isEditing ? -SE50 : 0)
+                    .offset(y:isEditing ? SE100 : 0).offset(y:-screen_height*0.38)
                 
                 
                 
                 
-                ScrollView(.vertical, showsIndicators: false){
-                    LazyVGrid(columns: twoColumnGrid, alignment:.center, spacing : 20) {
-                              // Display the item
-                     
-                        ForEach(category, id: \.self){ i in
-          
-                            if i.contains(search_text) || search_text == "" {
-     
-                                cardView(name: i)
-                                
-                                
-                            }
-                        }.animation(Animation.ripple(index: 3) , value: search_text)
-                            
-                        
-                    }
-                }
-            }
+               
+            
         }
             
             
